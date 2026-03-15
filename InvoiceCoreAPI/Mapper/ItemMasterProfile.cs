3f43b523973fe9638ca9ed7a7ad98c03d6a7563b
sasimaha0072005-code <sasimaha0072005@gmail.com>
2026-03-15 15:44:24 +0530

    first commit

---
+++ InvoiceCoreAPI/Mapper/ItemMasterProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper; 
+
+using InvoiceCoreAPI.Entities;
+
+using InvoiceCoreAPI.DTO;
+
+namespace InvoiceCoreAPI.Mapper
+
+{
+
+    public class ItemMasterProfile : Profile
+
+    {
+
+        public ItemMasterProfile()
+
+        {
+
+            CreateMap<Itemmaster, ItemmasterDto>().ReverseMap();
+
+        }
+
+    }
+
+}
+
