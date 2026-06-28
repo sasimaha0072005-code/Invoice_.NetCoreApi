fdcd5d3a874e3975d3df65f20cc3c932026ceda5
sasimaha0072005 <sasimaha0072005@gmail.com>
2026-06-28 16:45:50 +0530

    completed customer module

---
+++ InvoiceCoreAPI/Mapper/CustomerProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using InvoiceCoreAPI.Entities;
+using InvoiceCoreAPI.DTO;
+namespace InvoiceCoreAPI.Mapper
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+        }
+    }
+}
+
